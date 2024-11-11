50155dfdf73bbdae42a9f13d32dbdc2ed3cf07b2
Ali Emara <aliemara175@gmail.com>
2024-11-11 19:43:30 +0200

    Update mdelh.py
    expected 1 blank line before a nested definition, found 0 (E306)

--- mdelh.py
+++ mdelh.py
@@ -280,6 +280,7 @@ async def main():
     """ Main function that sets up the signal handler, loads API token from configuration file, reads IOCs file, and processes the items. """
     signal.signal(signal.SIGINT, handle_interrupt)
     # Load API key from configuration file
+    
     def load_config(filename):
         with open(filename, 'r') as file:
             return json.load(file)
